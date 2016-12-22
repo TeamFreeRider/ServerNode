21cb71992c6524b479045478906b6bc6fb6ae7b8
celinelee522 <celinelee522@gmail.com>
2016-12-22 17:32:21 +0900

    db_push practice

--- db_connect.js
+++ db_connect.js
@@ -2,6 +2,18 @@ var mongoose = require('mongoose');
 
 mongoose.connect("mongodb://celine:0522@ds157677.mlab.com:57677/freerider");
 
+//create object and instance
+var locationSchema = mongoose.Schema({
+
+    x : Number,
+    y : Number
+
+});
+
+var Location = mongoose.model('locations',locationSchema);
+
+Location.findOne().sort({x:-1});
+
 
 
 var db = mongoose.connection;
@@ -19,16 +31,3 @@ db.on("error",function(err) {
     console.log("db error :",err);
 
 });
-
-
-
-//create Object and make Instance
-
-var locationSchema = mongoose.Schema({
-
-    x : Number,
-
-    y : Number
-
-});
-
---
+++ db_push.js
@@ -0,0 +1,49 @@
+//console.time('before insert');
+
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://celine:0522@ds157677.mlab.com:57677/freerider");
+
+
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
+
+
+db.once("open",function() {
+
+    console.log("db connected");
+});
+
+db.on("error",function(err) {
+
+    console.log("db error :",err);
+});
+
+
+var LocationSchema = new Schema({
+    'x': Number,
+    'y': Number
+});
+
+var Location = mongoose.model('locations', LocationSchema);
+
+//console.timeEnd('before insert');
+//console.time('Insert Method');
+
+var location = new Location();
+location.x = 100;
+location.y = 44;
+location.save(function(err){
+    if(err) console.log("Something went wrong while saving the thing");
+    else console.log("successfully saved");
+});
+
+//console.timeEnd('Insert Method');
+//console.time('Find Method');
+
+Location.find({}, function(err,docs){
+    console.log(docs);
+});
+
+//console.timeEnd('Find Method');
+
+//Location.findOne().sort({x:-1});
